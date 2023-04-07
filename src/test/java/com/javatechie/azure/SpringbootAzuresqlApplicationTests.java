81021d8556b05c1aafa5680ae3c6234197ae9b6b
Polonski Iouri <iouri.polonski@e-velopment.de>
2023-04-07 13:33:04 +0200

    verbindung zu db in azure cloud

--- src/test/java/com/javatechie/azure/SpringbootAzuresqlApplicationTests.java
+++ src/test/java/com/javatechie/azure/SpringbootAzuresqlApplicationTests.java
@@ -1,9 +1,13 @@
 package com.javatechie.azure;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@SpringBootTest
+@ExtendWith(SpringExtension.class)
+@ActiveProfiles("test")
 class SpringbootAzuresqlApplicationTests {
 
 	@Test
